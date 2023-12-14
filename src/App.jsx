bb3c5dbde6bd16571830f045dc8ee7095fe6da04
Lucas Capdevila <lcapdevila60@gmail.com>
2023-12-14 18:31:08 -0300

    se agrega componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormTareas from './components/FormTareas';
 import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
-    <Container className='my-4'>
-      <h1 className='text-center display-2'>Lista de Tareas</h1>
-      <FormTareas />
-    </Container>
+    <>
+      <Container className='my-4'>
+        <h1 className='text-center display-2'>Lista de Tareas</h1>
+        <FormTareas />
+      </Container>
+
+      <Footer />
+    </>
   )
 }
 
