98055440b41ad54d2b0f7ef1e29f9495d7a775b2
Lucas Capdevila <lcapdevila60@gmail.com>
2023-12-14 17:35:11 -0300

    se agrega la librería de React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Hola mundo !</h1>
+      <h1>Hola mundo</h1>
     </>
   )
 }
